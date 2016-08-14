19a90c59d7529849a30f83c6fbf2bd4f763c6150
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-08-14 05:55:56 +0200

    WIP

--- cubing.el
+++ cubing.el
@@ -1,25 +1,63 @@
 (setq moves '("R" "U" "F" "L" "D" "B"))
+(setq moves-lower (mapcar 'downcase moves))
 (setq move-suffixes '("'" "2" ""))
 
-
-(defun char-is-move (c)
-  "Test whether the given char constitutes a valid move "
-  (member (upcase (char-to-string c)) moves))
+(defun string-is-move (s)
+  "Test whether the given string constitutes a valid move "
+  (member (upcase s) moves))
 
 (defun cubing-quote-last-move ()
   "docstring"
   (interactive)
-  (let ((nop nil) (no-space (not (eq (char-before) ?\ ))))
+  (let ((nop nil)
+        (no-space (not (eq (char-before) ?\ ))))
     (save-excursion
       (sp-backward-whitespace)
-      (if (char-is-move (char-before)) (insert "'") (set 'nop t)))
-    (if nop (insert "i")
+      (if (string-is-move (char-to-string (char-before)))
+          (insert "'")
+        (set 'nop t)))
+    (if nop
+        (insert "i")
       ;; if no fallback needed, make position cleanup
       (if no-space (forward-char) (delete-backward-char 1)))
     ))
 
+(defun raw-insert-move (move)
+  "Insert a move (str) as is"
+  (interactive)
+  (insert (upcase move))
+  )
 
-;; global quote binding
-;; (global-set-key (kbd "i") 'cubing-quote-last-move)
+(defun insert-move (move)
+  "Insert the move `move` (string) with context to the last move
+on that line."
+  (interactive)
+  (if (string-is-move move)
+      (save-excursion
+        (sp-backward-whitespace)
+        (if (string-equal (char-to-string (char-before)) move)
+            (progn (insert "2") (just-one-space))
+          (progn (just-one-space) (raw-insert-move move))))
+    nil))
+
+(defun self-insert-as-move (move)
+  "insert"
+  (interactive "K")
+  (message (format "got move %s" (move)))
+  (if (string-is-move move) (insert-move move) (self-insert-command 1))
+  )
+
+;; (mapcar (lambda (key-str) (local-set-key (kbd key-str) 'self-insert-as-move))
+;;         moves-lower)
+(mapcar (lambda (key-str) (local-set-key (kbd key-str) 'self-insert-command))
+        moves-lower)
+(local-set-key (kbd "u") 'self-insert-as-move)
+
+
+
+
+;; Quote binding
+;; (local-set-key (kbd "i") 'cubing-quote-last-move)
 ;; Backup binding
 ;; (global-set-key (kbd "i") 'self-insert-command)
+;; R U2 R'
