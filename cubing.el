9bbb4d2eff2df835c5e162324809df52f67da166
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-08-14 04:50:31 +0200

    Replace custom methodby `sp-backward-whitespace`
    Remove the custom `move-before-spaces` function.

--- cubing.el
+++ cubing.el
@@ -6,18 +6,12 @@
   "Test whether the given char constitutes a valid move "
   (member (upcase (char-to-string c)) moves))
 
-(defun move-before-spaces ()
-  "Go backwards as long as not on the beginning of the line and
-the previous char is space."
-  (interactive)
-  (while (char-equal (char-before) ?\ ) (backward-char)))
-
 (defun cubing-quote-last-move ()
   "docstring"
   (interactive)
   (let ((nop nil) (no-space (not (eq (char-before) ?\ ))))
     (save-excursion
-      (move-before-spaces)
+      (sp-backward-whitespace)
       (if (char-is-move (char-before)) (insert "'") (set 'nop t)))
     (if nop (insert "i")
       ;; if no fallback needed, make position cleanup
